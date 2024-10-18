f48508b97600f4a3d909e9361ebb0cbef31ff708
IgorOGregorio <igorgregoriomg@gmail.com>
2024-10-18 11:50:54 -0300

    fix: tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,11 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/jwtAuth.guard';
+import { AuthService } from './auth/services/auth.service';
+import { PrismaModule } from './prisma/Prisma.module';
+import { UrlModule } from './url/url.module';
+import { UserModule } from './user/user.module';
 
 describe('AppController', () => {
   let controller: AppController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule, UserModule, AuthModule, JwtModule, UrlModule],
+      providers: [
+        AuthService,
+        {
+          provide: APP_GUARD,
+          useClass: JwtAuthGuard,
+        },
+      ],
       controllers: [AppController],
     }).compile();
 
--- src/url/services/findUrlsByUserId/findUrlsByUserId.service.spec.ts
+++ src/url/services/findUrlsByUserId/findUrlsByUserId.service.spec.ts
@@ -1,18 +1,55 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { FindUrlsByUserIdService } from './findUrlsByUserId.service';
+import { urlModuleTest } from '../../utils/urlModuleTest';
+import { Url } from '../../entities/url.entity';
+import { IUrlRepository } from '../../repositories/iUrl.repository';
 
 describe('FindUrlsByEmailService', () => {
   let service: FindUrlsByUserIdService;
+  let urlRepository: IUrlRepository;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [FindUrlsByUserIdService],
-    }).compile();
+    const module = await urlModuleTest();
 
     service = module.get<FindUrlsByUserIdService>(FindUrlsByUserIdService);
+    urlRepository = module.get<IUrlRepository>(IUrlRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should find urls by user id', async () => {
+    const userId = crypto.randomUUID();
+    //mock repository
+    const urls = [
+      new Url(
+        {
+          url: 'https://www.google.com',
+          shortUrl: 'http://localhost:3000/1',
+          accessCount: 0,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          deletedAt: null,
+        },
+        crypto.randomUUID(),
+      ),
+      new Url(
+        {
+          url: 'https://www.linkedin.com',
+          shortUrl: 'http://localhost:3000/2',
+          accessCount: 0,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          deletedAt: null,
+        },
+        crypto.randomUUID(),
+      ),
+    ];
+
+    jest.spyOn(urlRepository, 'findUrlsByUserId').mockResolvedValue(urls);
+
+    const result = await service.execute(userId);
+
+    expect(result).toStrictEqual(urls);
   });
 });
